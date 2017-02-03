c7d28e5a23f48a4b47b0c2088257e56da477a3c1
Adam Rogulski <kontakt.nowy@gmail.com>
2017-02-03 23:13:22 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+
+	var link = document.getElementById('link');
+	link.onclick = function() {
+		alert('– mięsień skroniowy \n– mięsień policzkowy \n– mięsień żwacz \n– brzusiec czołowy mięśnia potyliczno-czołowego');
+	}
+
+	var link2 = document.getElementById('link2');
+	link2.onclick = function() {
+		alert('– mięsień piersiowy większy \n– mięsień piersiowy mniejszy');
+	}
+
+	var link3 = document.getElementById('link3');
+	link3.onclick = function() {
+		alert('– mięsień prosty brzucha \n– mięsień skośny zewnętrzny brzucha');
+	}
+
+	var link4 = document.getElementById('link4');
+	link4.onclick = function() {
+		alert('– mięsień naramienny \n– mięsień ramienno-promieniowy \n– mięsień zginacz palców powierzchowny');
+	}
+
+	var link5 = document.getElementById('link5');
+	link5.onclick = function() {
+		alert('– mięsień czworoboczny \n– mięsień najszerszy grzbietu \n– mięsień obły większy');
+	}
+
+	var link6 = document.getElementById('link6');
+	link6.onclick = function() {
+		alert('– mięsień prosty uda \n– mięsień obszerny boczny \n– mięsień biodrowo-lędźwiowy');
+	}
+}
