165f9a8546b895fd5bfa9882e208fd00b5284ae0
Adam Rogulski <kontakt.nowy@gmail.com>
2017-02-21 18:27:55 +0100

    Update js

--- javascript.js
+++ javascript.js
@@ -1,32 +1,18 @@
-window.onload = function(){
-
-	var link = document.getElementById('link');
-	link.onclick = function() {
-		alert('– mięsień skroniowy \n– mięsień policzkowy \n– mięsień żwacz \n– brzusiec czołowy mięśnia potyliczno-czołowego');
-	}
-
-	var link2 = document.getElementById('link2');
-	link2.onclick = function() {
-		alert('– mięsień piersiowy większy \n– mięsień piersiowy mniejszy');
-	}
-
-	var link3 = document.getElementById('link3');
-	link3.onclick = function() {
-		alert('– mięsień prosty brzucha \n– mięsień skośny zewnętrzny brzucha');
-	}
-
-	var link4 = document.getElementById('link4');
-	link4.onclick = function() {
-		alert('– mięsień naramienny \n– mięsień ramienno-promieniowy \n– mięsień zginacz palców powierzchowny');
-	}
-
-	var link5 = document.getElementById('link5');
-	link5.onclick = function() {
-		alert('– mięsień czworoboczny \n– mięsień najszerszy grzbietu \n– mięsień obły większy');
-	}
-
-	var link6 = document.getElementById('link6');
-	link6.onclick = function() {
-		alert('– mięsień prosty uda \n– mięsień obszerny boczny \n– mięsień biodrowo-lędźwiowy');
-	}
-}
+document.addEventListener("DOMContentLoaded", function() {
+  
+  var messages = [
+    '– mięsień skroniowy \n– mięsień policzkowy \n– mięsień żwacz',
+    '– mięsień piersiowy większy \n– mięsień piersiowy mniejszy',
+    '– mięsień prosty brzucha \n– mięsień skośny zewnętrzny brzucha',
+    '– mięsień naramienny \n– mięsień ramienno-promieniowy',
+    '– mięsień czworoboczny \n– mięsień najszerszy grzbietu',
+    '– mięsień prosty uda \n– mięsień obszerny boczny'
+   ];
+ 
+  $(".block").each(function(index) {
+    $(this).on("click", function(){
+      alert(messages[index])
+      
+    });  
+  }); 
+});
